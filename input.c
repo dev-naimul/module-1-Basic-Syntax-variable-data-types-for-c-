d57863500986406315122d488f2dd53a0b7c8c42
Naimul Islam Nahid <islamnaimul000@gmail.com>
2026-04-09 11:31:18 +0600

    module-1 of c language

---
+++ input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a,c;
+    float b;
+    scanf("%d %d %f",&a,&c,&b);
+    printf("%d %d %f", c,a,b);
+    
+}
---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   // printf("NAIMUL ISLAM NAHID");
+    int num = 2000000000;
+    long long int t= 20222222334455545; 
+    double v = 222.45655;
+
+    float f= 1.3333;
+    char c = 'q';
+    printf("%.2f %lld %lf",f,t,v);
+    return 0;
+}
